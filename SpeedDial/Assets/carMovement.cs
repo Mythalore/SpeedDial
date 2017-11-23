ff5d556c0979f644f7ee970870c13776808837b3
Ellie Richards <eleanorichards@hotmail.co.uk>
2017-11-23 11:38:25 +0000

    Here you go callan

--- SpeedDial/Assets/carMovement.cs
+++ SpeedDial/Assets/carMovement.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class carMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public float speed = 90f;
+    public float turnSpeed = 5f;
+
+    private float powerInput;
+    private float turnInput;
+    private Rigidbody carRigidbody;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        powerInput = Input.GetAxis("Vertical");
+        turnInput = Input.GetAxis("Horizontal");
+
+        //carRigidbody.AddForce(appliedForce, ForceMode.Acceleration);
+        // carRigidbody.AddRelativeForce(0f, 0f, powerInput * speed);
+        //carRigidbody.AddRelativeTorque(0f, turnInput * turnSpeed, 0f);
+    }
 
 
     void MoveCar()
