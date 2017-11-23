cf834c29db00df24b29d432646f6e4e6677de69a
Tipe125 <timpenfold@hotmail.co.uk>
2017-11-23 12:18:56 +0000

    Lobby and Menu functional, input manager added, needs new graphics

---
+++ SpeedDial/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+
+	public Animator animator;
+
+	public void StartGameBtn(string newGameLevel)
+	{
+		SceneManager.LoadScene (newGameLevel);
+	}
+
+	public void QuitGameBtn()
+	{
+		Application.Quit ();
+
+	}
+
+	public void ControlsBtn()
+	{
+		animator.SetBool ("ControlsUp", !animator.GetBool ("ControlsUp"));
+	}
+
+}
