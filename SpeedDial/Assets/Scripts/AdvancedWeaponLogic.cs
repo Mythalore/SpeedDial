dd71514651b750a9b1c8f6cb4d960963891af786
Dominic Hawkins <dominic2.hawkins@live.uwe.ac.uk>
2017-11-23 19:49:01 +0000

    Added Advanced weapon pick up, variable uses, weapon and attachment states, made rocket pick-up-able and fireable using the X button

---
+++ SpeedDial/Assets/Scripts/AdvancedWeaponLogic.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdvancedWeaponLogic : MonoBehaviour {
+    private int weaponUses;
+    private int weaponDamage;
+    private float timer = 1.0f;
+    private float timeBetweenShots = 1.0f;
+    public GameObject rocket;
+    private string AttachedWeaponName;
+    public enum AttachState
+    {
+        Idle,
+        NoWeaponAttached,
+        WeaponAttached
+    }
+    public enum WeaponState
+    {
+        Idle,
+        Tracking,
+        NotTracking,
+        Firing
+    }
+    public AttachState attachState;
+    public WeaponState weaponState;
+	// Use this for initialization
+	void Start () {
+        attachState = AttachState.NoWeaponAttached;
+        weaponState = WeaponState.Idle;
+        timer = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton("AdvFire1") && attachState == AttachState.WeaponAttached)
+        {
+            timer += Time.deltaTime;
+            if (timer >= timeBetweenShots)
+            {
+                timer = 0.0f;
+                if (AttachedWeaponName == "Rocket")
+                {
+                    var newBullet = Instantiate(rocket, gameObject.transform.position, this.transform.rotation);
+                    newBullet.transform.parent = gameObject.transform.parent;
+                    weaponUses--;
+                }
+                if (weaponUses == 0)
+                {
+                    removeWeapon();
+                }
+            }
+
+        }
+	}
+    public void AttachedWeapon(string weaponType)
+    {
+        attachState = AttachState.WeaponAttached;
+        if (weaponType == "Rocket")
+        {
+            AttachedWeaponName = "Rocket";
+            weaponUses = 3;
+            weaponDamage = 20;
+        }
+    }
+    private void removeWeapon()
+    {
+        attachState = AttachState.NoWeaponAttached;
+        Destroy(gameObject.transform.Find(AttachedWeaponName).gameObject);
+    }
+
+}
---
+++ SpeedDial/Assets/Scripts/PickUpPower.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpPower : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Player1") && col.gameObject.GetComponentInChildren<AdvancedWeaponLogic>().attachState == AdvancedWeaponLogic.AttachState.NoWeaponAttached)
+        {
+            transform.parent = col.transform;
+            var pos = col.transform.Find("AttachmentPoint");
+            transform.position = pos.position;
+            transform.parent = pos;
+            pos.GetComponent<AdvancedWeaponLogic>().AttachedWeapon(gameObject.tag);
+        }
+
+    }
+}
