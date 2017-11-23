e3842b4f090d0a5adc21d5a79983ecb9ee1a8199
Dominic Hawkins <dominic2.hawkins@live.uwe.ac.uk>
2017-11-23 18:15:02 +0000

    Added firing basic weapon to the A button, fires a bullet  that destroys when hitting a barrel or the ground. damage can be added when that is added to players

---
+++ SpeedDial/Assets/Scripts/BasicWeaponLogic.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicWeaponLogic : MonoBehaviour {
+    public GameObject bullet;
+    private float time = 0.0f;
+    private float timeBetweenBullet = 0.5f;
+	// Use this for initialization
+	void Start () {
+        time = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("Fire1"))
+        {
+            time += Time.deltaTime;
+            if (time >= timeBetweenBullet)
+            {
+                time = 0.0f;
+                var newBullet = Instantiate(bullet, gameObject.transform.position, this.transform.rotation);
+                newBullet.transform.parent = gameObject.transform.parent;
+            }
+
+        }
+	}
+}
---
+++ SpeedDial/Assets/Scripts/BulletLogic.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletLogic : MonoBehaviour {
+    private float speed = 30.0f;
+    private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.velocity = transform.forward * speed;
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag != transform.parent.tag)
+        {
+            if (col.CompareTag("Ground") || col.CompareTag("Barrel"))
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                //Damage that player
+            }
+
+        }
+    }
+
+}
