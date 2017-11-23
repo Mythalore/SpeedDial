014fc7b77a5fdcacc1131c13550384a223ce317b
Ellie Richards <eleanorichards@hotmail.co.uk>
2017-11-23 11:33:26 +0000

    Added some things for starting

---
+++ SpeedDial/Assets/Wheels.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wheels : MonoBehaviour {
+    private Vector3 rotation_speed = new Vector3(0.0f,0.0f,2.0f);
+    private GameObject[] wheels;
+
+	// Use this for initialization
+	void Start ()
+    {
+        wheels = GameObject.FindGameObjectsWithTag("Wheels");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //if acceleration down
+        for(int i = 0; i < wheels.Length; i++)
+        { 
+            wheels[i].transform.Rotate(rotation_speed);
+        }
+	}
+}
