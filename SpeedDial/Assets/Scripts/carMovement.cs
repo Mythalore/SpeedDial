d02c66e4e9ee9a6857966cb8671f32d6472300c0
callan-patay <Callan2.Patay@live.uwe.ac.uk>
2017-11-23 12:20:56 +0000

    car moves nd tingz

---
+++ SpeedDial/Assets/Scripts/carMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carMovement : MonoBehaviour {
+
+    public float speed_modifier = 20;
+    public float turn_modifier = 10;
+    private float powerInput;
+    private float turnInput;
+    private Rigidbody carRigidbody;
+    public float threshold = 0;
+    public float dist_to_ground = 0.5f;
+
+    // Use this for initialization
+    void Start () {
+        carRigidbody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        MoveCar();
+    }
+
+
+    void MoveCar()
+    {
+        Vector3 down = -Vector3.up;
+     
+        powerInput = Input.GetAxis("Vertical") * speed_modifier;
+        turnInput = Input.GetAxis("Horizontal") * turn_modifier;
+
+        if (powerInput < threshold)
+        {
+            turnInput *= 0.1f;
+        }
+
+        carRigidbody.AddRelativeForce(0f, 0f, powerInput);
+        carRigidbody.AddRelativeTorque(0f, turnInput, 0f);
+    }
+}
