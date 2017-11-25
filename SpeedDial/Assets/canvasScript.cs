030638386ed1dbb40d127eabae195f7bd1bfee1f
harryxa <Harry2.Alcott@live.uwe.ac.uk>
2017-11-25 11:34:21 +0000

    textin start

---
+++ SpeedDial/Assets/canvasScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class canvasScript : MonoBehaviour {
+
+    // Use this for initialization
+
+    public Image phone;
+	void Start () {
+		//phone_screen_text = canvas.getcomponentInchildren<Text>
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if(Input.GetKeyDown("joystick button 0"))
+        {
+
+            
+            //canvas.getcomponentinchgildren<Image>(("A button")= a_button_highlighted;
+            //correct_button_down = true;
+            //ifcorrect_button_down
+            //if right_dpad
+            ////WELL DONE
+            //fireweapon = true;
+           
+        }	
+	}
+
+    
+}
