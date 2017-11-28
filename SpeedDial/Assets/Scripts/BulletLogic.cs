1c346b0498b31dec685117f3ebb4d160bc7d8182
Callan Patay <callan2.patay@live.uwe.ac.uk>
2017-11-28 19:32:06 +0000

    Change car prefabs and changed advanced weapon logic

--- SpeedDial/Assets/Scripts/BulletLogic.cs
+++ SpeedDial/Assets/Scripts/BulletLogic.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletLogic : MonoBehaviour {
-    private float speed = 450.0f;
+    private float speed = 3000.0f;
     private Rigidbody rb;
 	// Use this for initialization
 	void Start () {
